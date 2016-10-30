f3645861f6240fdd02f8977498d0155e96e07f32
pmsosa <sosavpm@gmail.com>
2016-10-29 17:01:41 -0700

    LWE: Modified A's creation to contain actually big numbers
    RLWE: Added RLWE PKC code. It works with a small caveat that the creation of polynomials is not as efficient as it could be since sometimes it creates polynomials with max exponent less than N

--- lwe_pkc.py
+++ lwe_pkc.py
@@ -27,7 +27,8 @@ Alice 								Bob
 
 def gen_A(n,q):
 	location = 0 #Gamma Distribution Location (Mean "center" of dist.)
-	a = np.floor(np.random.normal(0,size=(n,n))%q)
+	#a = np.floor(np.random.normal(0,size=(n,n))%q)
+	a = np.floor(np.random.random(size=(n,n))*q)%q
 	return np.matrix(a);
 
 def gen_vect(n):
@@ -49,18 +50,18 @@ bit = 1 # Bit that bob wants to send to alice
 
 #Step 1. (UNIVERSE) Generate A
 A = gen_A(n,q) # N*N matrix whith integer matrices that are {0, ..., q-1}
-print "A: ", A
+#print "A: ", A
 
 #Step 2. (ALICE)
 s = gen_vect(n)	#Alice Generate Secret <s1 .... sn>
 e = gen_vect(n)	#Alice Generate Error  <e1 .....en>
-print "S: ",s,"E:",e
+#print "S: ",s,"E:",e
 
 
 ## St       A
 ## (1*5)  (5*5)
 ut = np.transpose(s)*A + np.transpose(e) #Alice send to Bob (Public Key)
-print "ut: ",ut
+#print "ut: ",ut
 
 
 
@@ -70,15 +71,19 @@ x = gen_vect(n)   #Bob Generate Error  <x1 .....xn>
 xp = gen_num()    #Bob Generates x' Error
 
 b = A*r + x #Bob sends to Alice (CipherText Preamble)
-print "b:",b
+#print "b:",b
 bp = ut*r + xp + bit*np.floor(q/2) #Bob sends to Alice (Payload)
+#print "bp",bp
 
 
 
 #Step 4. (ALICE)
 info = bp - np.transpose(s)*b 
 
-print "------"
-print "Alice:",info," ~~ Bob:",bit*np.floor(q/2)
 
+print "------"
+print "Alice:",float(info[0][0])," ~~ Bob:",bit*np.floor(q/2)
+if (np.round((bit*np.floor(q/2))/info) == bit):
+	print "Agreed secret:", bit
+print "------"
 
